164b7dd040314d45af049e75dae3dd9fd0f52b0b
VorobevDenis <Den001v@gmail.com>
2021-12-10 14:52:05 +0700

    Исключен из работы файл apps.py, к приложению происходит обращение напрямую

--- multipage_form_demo/settings.py
+++ multipage_form_demo/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'job_application.apps.JobApplicationConfig'
+    'job_application'
 ]
 
 MIDDLEWARE = [
